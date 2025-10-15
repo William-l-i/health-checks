a85c5f31f2d9625e3c07ee0decb746151c55b604
wlin@nbsps.com <wlin@nbsps.com>
2025-10-15 15:55:10 -0400

    Create wrapper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,9 @@
 #!/usr/binenv python3
 (...)
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
